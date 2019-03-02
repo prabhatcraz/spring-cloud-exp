85eb7785ac9a7cb314bde1c368cd83d8bf0b6eba
Prabhat Ranjan <prabhat.ranjan@ing.com>
2019-03-02 09:29:56 +0100

    Working model of API gateway, Eureka and a beer microservice

--- beers/src/main/java/com/spring/cloud/practice/beers/BeersController.java
+++ beers/src/main/java/com/spring/cloud/practice/beers/BeersController.java
@@ -1,5 +1,6 @@
 package com.spring.cloud.practice.beers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,9 +8,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+@Slf4j
 @RestController
 public class BeersController {
-  List<String> beers = new ArrayList<String>() {{
+  private final List<String> beers = new ArrayList<String>() {{
     this.add("heineken");
     this.add("Guinness");
     this.add("Blue Moon");
@@ -22,6 +24,8 @@ public class BeersController {
   @RequestMapping(path = "/beer")
   public String getAJoke() {
     final Random random = new Random();
-    return beers.get(random.nextInt(7));
+    final  String beerName = beers.get(random.nextInt(7));
+    log.info("Beer request {}", beerName);
+    return beerName;
   }
 }
